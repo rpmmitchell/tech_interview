f7f56e094ef2bd8ce0325db333019b8a0f315acc
Ryan <rpmmitchell@outllok.com>
2021-11-05 09:20:40 -0500

    first array string problem

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# coding=utf-8
-# This is a sample Python script.
+def checkUni(checkMe):
+    uniDict = {}
+    for count, value in enumerate(checkMe):
+        uniDict[value] = count;
+    print(uniDict)
+    if len(uniDict) != len(checkMe):
+        print('I do not have all unique values')
+    else:
+        print('I have all unique values')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+checkUni('heloo')
